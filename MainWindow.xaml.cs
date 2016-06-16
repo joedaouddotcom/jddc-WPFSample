272fa97afbd4455f0a686e360be45e3facc83203
Joe Daoud <joedaouddotcom@users.noreply.github.com>
2016-06-16 16:14:05 -0700

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace WPFEvents
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            button.Click += button_MyOtherClick;
+
+        }
+
+        private void button_MyOtherClick(object sender, RoutedEventArgs e)
+        {
+            myOtherLabel.Content = "Copyright 2016 joedaouddotcom";
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            // Github commit 
+            myLabel.Content = "Hello world!";
+            myLabel.Opacity = 100;
+
+
+
+        }
+    }
+}
